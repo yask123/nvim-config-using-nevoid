3ce826279b4f7e7f3976689241171e5354e78e1c
Yask Srivastava <yask123@gmail.com>
2025-03-14 18:32:56 -0400

    Add Treesitter-based folding for better code structure

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Use treesitter for better folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- Don't fold by default when opening files
+vim.opt.foldenable = false
